379967439ae34248168d419330b0a9ce0bd07337
hotnick123 <6547123@naver.com>
2021-08-13 21:29:59 +0900

    first commit

---
+++ src/main/java/com/pettalk/pettalkbackend/PetTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pettalk.pettalkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTalkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pettalk/pettalkbackend/PetTalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pettalk.pettalkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
